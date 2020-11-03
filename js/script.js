2b421dd119835b8964ad2743f4d1ee2c84a7b443
Andrey <andrey.zhelkovsky@gmail.com>
2020-11-02 20:27:41 -0500

    adding more details for JS

--- js/script.js
+++ js/script.js
@@ -39,3 +39,64 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+// Условия
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
+
+// Вложеные условия
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error!');
+// } else if (num > 100) {
+//     console.log('Too Much');
+// } else {
+//     console.log('Ok!');
+// }
+
+// Использование тернарного оператора (conditional (ternary) operator)
+// const num = 51;
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+// Использование switch
+
+// const num = 50;
+// switch (num) {
+//     case 49:
+//         console.log('Error');
+//         break;
+//     case 100:
+//         console.log('Error');
+//         break;
+//     case 50:
+//         console.log('Bullseye!');
+//         break;
+//     default :
+//         console.log('Maybe next time!');
+//         break;    
+// }
+
+// Циклы - Loops & Iterations
+// WHILE
+
+// let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// LOOP DO
+let num = 50;
+do {
+    console.log(num);
+    num++;
+}
+while (num <= 55);
+
+// LOOP 
